741ec369970b2f36a664e534fe5e583f4019e4ba
DelhinRharl <delhinrharl@gmail.com>
2022-02-22 23:34:48 +0100

    Add data,schema and queries

--- data.sql
+++ data.sql
@@ -1,5 +1,15 @@
-/* Populate database with sample data. */
+INSERT INTO animals
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon', '3-2-2020', '0', 'true', '10.23');
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals
+(name, date_of_birth, escape_attempts, neutered, weight_Kg)
+VALUES ('Gabumon', '11-15-2018', '2', "true", '8')
+
+INSERT INTO animals
+(name, date_of_birth, escape_attempts, neutered, weight_Kg)
+VALUES ('Pikachu', '7-1-2021', '1', 'false', '15.04')
+
+INSERT INTO animals
+(name, date_of_birth, escape_attempts, neutered, weight_Kg)
+VALUES ('Devimon', '12-5-2017', '5', 'true', '11')
