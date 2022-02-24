3386fc7f137b773f7d5ced2a3d694dfc85bc7480
DelhinRharl <delhinrharl@gmail.com>
2022-02-24 14:18:53 +0100

    Add tables to the database/Add data and queris

--- data.sql
+++ data.sql
@@ -34,8 +34,64 @@ VALUES ('Angemon', '2005-06-12', '1', 'true', '-45');
 
 INSERT INTO animals
 (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Boarmon', '2005-06-07', '7', 'true', '20.4');
+VALUES ('Boarmon', '2005-06-07', '7', 'TRUE', '20.4');
 
 INSERT INTO animals
 (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Blossom', '1998-10-13', '3', 'true', '17');
+VALUES ('Blossom', '1998-10-13', '3', 'TRUE', '17');
+
+--third batch
+
+INSERT INTO owners (
+    full_name,
+    age
+  )
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (
+    full_name,
+    age
+  )
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (
+    full_name,
+    age
+  )
+VALUES ('Bob', 45);
+
+INSERT INTO owners (
+    full_name,
+    age
+  )
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (
+    full_name,
+    age
+  )
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (
+    full_name,
+    age
+  )
+VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (
+    name
+  )
+VALUES ('Pokemon');
+
+INSERT INTO species (
+    name
+  )
+VALUES ('Digimon');
+
+UPDATE animals SET species_id=2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id=1 WHERE species_id IS NULL;
+UPDATE animals SET owners_id=1 WHERE name='Agumon';
+UPDATE animals SET owners_id=2 WHERE name='Gabumon' OR name='Pikachu';
+UPDATE animals SET owners_id=3 WHERE name='Devimon' OR name='Plantmon';
+UPDATE animals SET owners_id=4 WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owners_id=5 WHERE name='Angemon' OR name='Boarmon';
