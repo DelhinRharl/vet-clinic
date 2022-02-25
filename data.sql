5d73007a51112fd18a2be83e6aa3fccf34ab75aa
DelhinRharl <delhinrharl@gmail.com>
2022-02-25 10:59:44 +0100

    Add visits and specialization data

--- data.sql
+++ data.sql
@@ -95,3 +95,63 @@ UPDATE animals SET owners_id=2 WHERE name='Gabumon' OR name='Pikachu';
 UPDATE animals SET owners_id=3 WHERE name='Devimon' OR name='Plantmon';
 UPDATE animals SET owners_id=4 WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
 UPDATE animals SET owners_id=5 WHERE name='Angemon' OR name='Boarmon';
+
+--vets/specializations
+
+INSERT INTO vets (
+  name,
+   age, date_of_graduation) 
+VALUES('William Tatcher', 45, '2000-04-23'); 
+
+INSERT INTO vets (
+  name,
+  age, date_of_graduation) 
+VALUES('Maisy Smith', 26, '2019-01-17');
+
+INSERT INTO vets(
+  name, 
+  age, date_of_graduation
+) 
+VALUES('Stephanie Mendez', 64, '1981-04-05');
+
+INSERT INTO vets (
+  name, age, 
+  date_of_graduation) 
+VALUES
+('Jack Harkness', 38, '2008-08-06');
+
+
+INSERT INTO specializations (
+    species_id,
+    vets_id
+    ) VALUES
+    (1, 1),
+    (1, 3),
+    (2, 3),
+    (2, 4);
+
+    -- animal visits
+
+INSERT INTO visits  (animals_id, vets_id, date_of_visit) 
+VALUES
+
+    ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-5-24'),
+    ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-7-22'),
+    ((SELECT id FROM animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-2-2'),
+    ((SELECT id FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-1-5'),
+    ((SELECT id FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-3-8'),
+    ((SELECT id FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-5-14'),
+    ((SELECT id FROM animals WHERE name = 'Devimon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2021-5-4'),
+    ((SELECT id FROM animals WHERE name = 'Charmander'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-2-24'),
+    ((SELECT id FROM animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-12-21'),
+    ((SELECT id FROM animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-8-10'),
+    ((SELECT id FROM animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2021-4-7'),
+    ((SELECT id FROM animals WHERE name = 'Squirtle'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2019-9-29'),
+    ((SELECT id FROM animals WHERE name = 'Angemon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2020-10-3'),
+    ((SELECT id FROM animals WHERE name = 'Angemon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2020-11-4'),
+    ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-1-24'),
+    ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-5-15'),
+    ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-2-27'),
+    ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-8-3'),
+    ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-5-24'),
+    ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-1-11');
--- queries.sql
+++ queries.sql
@@ -80,3 +80,66 @@ WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
 
 SELECT owners.full_name, COUNT(animals.name) AS num_animals FROM owners
 LEFT JOIN animals ON owners.id = animals.owners_id GROUP BY owners.full_name ORDER BY num_animals DESC;
+
+--visits
+
+--William Thatcher last visit
+
+SELECT vet.name AS vet_name, an.name AS animal_name
+  FROM visits v JOIN animals an ON an.id = v.animal_id
+  JOIN vets vet ON vet.id = v.vet_id
+  WHERE vet.name = 'William Tatcher'
+  ORDER BY v.date_of_visit DESC LIMIT 1;
+
+-- Stephanie Mendez  animal visits
+SELECT vet.name as vet_name, COUNT(date_of_visit) 
+  FROM visits v JOIN vets vet ON v.vet_id = vet.id
+  WHERE name = 'Stephanie Mendez' GROUP BY vet.name;
+
+  --all vets and specialties
+
+  SELECT 
+	vets.name,
+	species.name AS specialized_in
+FROM vets
+LEFT JOIN specializations ON specializations.vets_id = vets.id
+LEFT JOIN  species ON specializations.species_id = species.id;
+
+--Stephanie Mendez 2020-04-01-2020-08-30
+SELECT animals.name AS animal_name, visits.date_of_visit FROM animals
+JOIN visits ON visits.animals_id = animals.id
+JOIN vets ON vets.id = visits.vets_id
+WHERE vets.name = 'Stephanie Mendez' AND visits.date_of_visit >= '2020-04-01' AND visits.date_of_visit <= '2020-08-30';
+
+--most visits 
+SELECT an.name, COUNT(an.name) AS most_visits
+  FROM visits v JOIN animals an ON an.id = v.animal_id
+  GROUP BY an.name ORDER BY most_visits DESC LIMIT 1;
+
+  --first visit Maisy Smith
+  SELECT an.name AS animal, vet.name AS vet, v.date_of_visit
+  FROM visits v JOIN animals an ON an.id = v.animal_id
+  JOIN vets vet ON vet.id = v.vet_id
+  WHERE vet.name = 'Maisy Smith' ORDER BY v.date_of_visit ASC LIMIT 1;
+
+  --most recent visit
+  SELECT an.id AS animal_id, an.name AS animal, an.date_of_birth, vet.id AS vet_id, vet.name AS vet, vet.age AS vet_age, date_of_visit
+  FROM visits v JOIN animals an ON an.id = v.animal_id
+  JOIN vets vet ON vet.id = v.vet_id ORDER BY v.date_of_visit DESC
+  LIMIT 1;
+
+    --vet visit -no specialization
+    SELECT COUNT(*)
+FROM visits
+JOIN animals ON animals.id = visits.animals_id
+JOIN vets ON vets.id = visits.vets_id
+JOIN specializations ON specializations.vets_id = visits.vets_id
+WHERE animals.species_id != specializations.species_id;
+
+--Maisy smith specialty to consider
+SELECT vet.name AS vet, sp.name AS species, COUNT(sp.name)
+  FROM visits v JOIN animals a ON a.id = v.animal_id
+  JOIN vets vet ON vet.id = v.vet_id
+  JOIN species sp ON sp.id = a.species_id
+  WHERE vet.name = 'Maisy Smith' GROUP BY vet.name, sp.name
+  ORDER BY COUNT DESC LIMIT 1;
